f49248156e374b4c4633d9af7054f420432572ca
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2019-10-31 15:24:03 +0100

    Entity Framework Introduction - Exercise

---
+++ Entity Framework Core - October 2019/03.Entity Framework Introduction - Exercise/02.Database First/StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace SoftUni
+{
+    using SoftUni.Data;
+    public class StartUp
+    {
+       public static void Main()
+        {
+            var context = new SoftUniContext();
+        }
+    }
+}
