29764186191db5923f109f18efa71e82d855571c
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2019-11-01 07:58:27 +0100

    03.Employees Full Information

---
+++ Entity Framework Core - October 2019/03.Entity Framework Introduction - Exercise/03.Employees Full Information/StartUp.cs
@@ -0,0 +1,43 @@
+﻿namespace SoftUni
+{
+    using SoftUni.Data;
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+       public static void Main()
+        {
+            SoftUniContext context = new SoftUniContext();
+
+          string result =  GetEmployeesFullInformation(context);
+
+            Console.WriteLine(result);
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context
+                .Employees
+                .Select(e => new
+                {
+                    Id = e.EmployeeId,
+                    Name = String.Join(" ", e.FirstName, e.LastName, e.MiddleName),
+                    e.JobTitle,
+                    e.Salary
+                })
+                .OrderBy(e => e.Id);
+
+            foreach (var e in employees)
+            {
+                sb
+                    .AppendLine($"{e.Name} {e.JobTitle} {e.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
