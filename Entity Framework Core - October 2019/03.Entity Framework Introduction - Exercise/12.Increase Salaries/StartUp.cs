de9bc86b58ca651701ec0e899c74fcfb3c69faba
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2019-11-03 21:58:17 +0100

    Entity Framework Core - 12.Increase Salaries

---
+++ Entity Framework Core - October 2019/03.Entity Framework Introduction - Exercise/12.Increase Salaries/StartUp.cs
@@ -0,0 +1,57 @@
+﻿namespace SoftUni
+{
+    using SoftUni.Data;
+    using SoftUni.Models;
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (var context = new SoftUniContext())
+            {
+                string result = IncreaseSalaries(context);
+
+                Console.WriteLine(result);
+            }
+        }
+
+        public static string IncreaseSalaries(SoftUniContext context)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            var employees = context
+                .Employees
+                .Where(e => e.Department.Name == "Engineering" ||
+                e.Department.Name == "Tool Design" ||
+                e.Department.Name == "Marketing" ||
+                e.Department.Name == "Information Services");
+
+            foreach (var e in employees)
+            {
+                e.Salary *= 1.12m;
+            }
+
+            context.SaveChanges();
+
+            var employeesToPrint = employees
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.Salary
+                })
+                .OrderBy(e => e.FirstName)
+                .ThenBy(e => e.LastName);
+
+            foreach (var e in employeesToPrint)
+            {
+                stringBuilder
+                    .AppendLine($"{e.FirstName} {e.LastName} (${e.Salary:f2})");
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
