fa0a85cc175b736542fceed86ea0b7d79d417782
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2019-11-06 06:54:16 +0100

    Entity Framework Introduction - Exercise

---
+++ Entity Framework Core - October 2019/03.Entity Framework Introduction - Exercise/08.Addresses By Town/StartUp.cs
@@ -0,0 +1,48 @@
+﻿namespace SoftUni
+{
+    using SoftUni.Data;
+    using SoftUni.Models;
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (var context = new SoftUniContext())
+            {
+                string result = GetAddressesByTown(context);
+
+                Console.WriteLine(result);
+            }
+        }
+
+        public static string GetAddressesByTown(SoftUniContext context)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            var addresses = context
+                .Addresses
+                .Select(a => new
+                {
+                    AddressText = a.AddressText,
+                    TownName = a.Town.Name,
+                    EmployeesCount = a.Employees.Count
+                })
+                .OrderByDescending(e => e.EmployeesCount)
+                .ThenBy(e => e.TownName)
+                .ThenBy(e => e.AddressText)
+                .Take(10)
+                .ToList();
+
+            foreach (var a in addresses)
+            {
+                stringBuilder
+                    .AppendLine($"{a.AddressText}, {a.TownName} - {a.EmployeesCount} employees");
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Entity Framework Core - October 2019/03.Entity Framework Introduction - Exercise/15.Remove Town/StartUp.cs
@@ -0,0 +1,50 @@
+﻿namespace SoftUni
+{
+    using SoftUni.Data;
+    using SoftUni.Models;
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (var context = new SoftUniContext())
+            {
+                string result = RemoveTown(context);
+
+                Console.WriteLine(result);
+            }
+        }
+
+
+        public static string RemoveTown(SoftUniContext context)
+        {
+            var seattle = context
+                .Towns
+                .First(t => t.Name == "Seattle");
+
+            var addressesInTown = context
+                .Addresses
+                .Where(a => a.Town == seattle);
+
+            var employeesToRemoveAddress = context
+                .Employees
+                .Where(e => addressesInTown.Contains(e.Address));
+
+            foreach (var e in employeesToRemoveAddress)
+            {
+                e.AddressId = null;
+            }
+
+            context.Addresses.RemoveRange(addressesInTown);
+
+            int addressesCount = addressesInTown.Count();
+
+            context.Towns.Remove(seattle);
+
+            return $"{addressesCount} addresses in Seattle were deleted";
+        }
+    }
+}
