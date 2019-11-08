9f82a9080da593f01ce797e32c999b119e2fa5a2
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2019-11-08 18:23:15 +0100

    01.Hospital Database

---
+++ Entity Framework Core - October 2019/04.Code First - Exercise/P01_HospitalDatabase/Data/HospitalContext.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_HospitalDatabase.Data.Models;
+
+namespace P01_HospitalDatabase.Data
+{
+    public class HospitalContext : DbContext
+    {
+        public HospitalContext()
+        {
+
+        }
+
+        public HospitalContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Diagnose> Diagnoses { get; set; }
+
+        public DbSet<Medicament> Medicaments { get; set; }
+
+        public DbSet<Patient> Patients { get; set; }
+
+        public DbSet<PatientMedicament> PatientsMedicaments { get; set; }
+
+        public DbSet<Visitation> Visitations { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Configuration.ConnectionString);
+            }
+            //base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Patient>(entity =>
+            {
+                entity.HasKey(p => p.PatientId);
+
+                entity.Property(p => p.FirstName)
+                .HasMaxLength(50)
+                .IsUnicode(true)
+                .IsRequired(true);
+
+                entity.Property(p => p.LastName)
+                .HasMaxLength(50)
+                .IsUnicode(true)
+                .IsRequired(true);
+
+                entity.Property(p => p.Address)
+                .HasMaxLength(250)
+                .IsUnicode(true)
+                .IsRequired(true);
+
+                entity.Property(p => p.Email)
+                .HasMaxLength(80)
+                .IsUnicode(true)
+                .IsRequired(true);
+
+                entity.Property(p => p.HasInsurance)
+                .IsRequired(true);
+
+                entity.HasMany(p => p.Visitations).WithOne(v => v.Patient);
+
+                entity.HasMany(p => p.Diagnoses).WithOne(d => d.Patient);
+
+
+
+            });
+
+            modelBuilder.Entity<Diagnose>(entity =>
+            {
+                entity.HasKey(d => d.DiagnoseId);
+
+                entity.Property(d => d.Name)
+                .HasMaxLength(50)
+                .IsRequired(true)
+                .IsUnicode(true);
+
+                entity.Property(d => d.Comments)
+                .HasMaxLength(250)
+                .IsUnicode(true);
+            });
+
+            modelBuilder.Entity<Medicament>(entity =>
+            {
+                entity.HasKey(m => m.MedicamentId);
+
+                entity.Property(m => m.Name)
+                .HasMaxLength(50)
+                .IsRequired(true)
+                .IsUnicode(true);
+            });
+
+            modelBuilder.Entity<PatientMedicament>(entity =>
+            {
+                entity.HasKey(pm => new
+                {
+                    pm.PatientId,
+                    pm.MedicamentId
+                });
+
+                entity.HasOne(pm => pm.Patient)
+                .WithMany(p => p.Prescriptions)
+                .HasForeignKey(pm => pm.PatientId);
+
+                entity.HasOne(pm => pm.Medicament)
+                .WithMany(m => m.Prescriptions)
+                .HasForeignKey(pm => pm.MedicamentId);
+            });
+        }
+    }
+}
---
+++ Entity Framework Core - October 2019/04.Code First - Exercise/P01_HospitalDatabase/Data/Models/PatientMedicament.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace P01_HospitalDatabase.Data.Models
+{
+    //Mapping class between Patients and Medicaments
+    public class PatientMedicament
+    {
+        public PatientMedicament()
+        {
+
+        }
+        public int PatientId { get; set; }
+        public Patient Patient { get; set; }
+
+        public int MedicamentId { get; set; }
+        public Medicament Medicament { get; set; }
+        public ICollection<PatientMedicament> Prescriptions { get; set; } = new HashSet<PatientMedicament>();
+    }
+}
