fad0e9f72b21030f4148e950a06a31d12cb0fe24
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2019-11-09 18:06:18 +0100

    Entity Relations - Football Betting.

---
+++ Entity Framework Core - October 2019/05.Entity Relations - Exercise/03.Football Betting/P03_FootballBetting.Models/Game.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P03_FootballBetting.Data.Models
+{
+    public class Game
+    {
+        public int GameId { get; set; }
+
+        public int HomeTeamId { get; set; }
+        //Nav prop
+        public int AwayTeamId { get; set; }
+        //Nav prop
+        public int HomeTeamGoals { get; set; }
+
+        public int AwayTeamGoals { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public double HomeTeamBetRate { get; set; }
+
+        public double AwayTeamBetRate { get; set; }
+
+        public double DrawBetRate { get; set; }
+
+        public string Result { get; set; }
+
+
+    }
+}
