7ef592e1b37486b71eb37d6390bcd5bb7c875fd5
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2019-11-21 21:47:38 +0100

    Auto Mapping Objects - Exercise

---
+++ Entity Framework Core - October 2019/08.Entity Framework Core - Auto Mapping Objects - Exercises/FastFood.Web/MappingConfiguration/FastFoodProfile.cs
@@ -0,0 +1,42 @@
+﻿namespace FastFood.Web.MappingConfiguration
+{
+    using AutoMapper;
+    using FastFood.Web.ViewModels.Categories;
+    using FastFood.Web.ViewModels.Employees;
+    using FastFood.Web.ViewModels.Items;
+    using Models;
+
+    using ViewModels.Positions;
+
+    public class FastFoodProfile : Profile
+    {
+        public FastFoodProfile()
+        {
+            //Positions x => replace source, y => replace destination.
+            this.CreateMap<CreatePositionInputModel, Position>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.PositionName));
+
+            this.CreateMap<Position, PositionsAllViewModel>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.Name));
+
+            this.CreateMap<Position, RegisterEmployeeViewModel>()
+                .ForMember(x => x.PositionId, y => y.MapFrom(x => x.Id))
+                .ForMember(x => x.PositionName, y => y.MapFrom(x => x.Name));
+
+            //Employees
+            this.CreateMap<RegisterEmployeeInputModel, Employee>();
+
+            this.CreateMap<Employee, EmployeesAllViewModel>()
+                .ForMember(x => x.Position, y => y.MapFrom(x => x.Position.Name));
+
+            //Categories
+            this.CreateMap<CreateCategoryInputModel, Category>()
+                .ForMember(x => x.Name, y => y.MapFrom(x => x.CategoryName));
+
+            this.CreateMap<Category, CategoryAllViewModel>();
+
+            this.CreateMap<Category, CreateItemViewModel>()
+                .ForMember(x => x.CategoryId, y => y.MapFrom(x => x.Id));
+        }
+    }
+}
