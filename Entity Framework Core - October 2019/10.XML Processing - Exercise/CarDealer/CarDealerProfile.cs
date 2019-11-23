ef8e619970d1df2984d8e96db0f5faf9e4a8bad4
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2019-11-23 21:09:22 +0100

    XML Processing - Exercise

---
+++ Entity Framework Core - October 2019/10.XML Processing - Exercise/CarDealer/CarDealerProfile.cs
@@ -0,0 +1,15 @@
+﻿using CarDealer.Dtos.Import;
+using CarDealer.Models;
+
+using AutoMapper;
+
+namespace CarDealer
+{
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            this.CreateMap<ImportSupplierDto, Supplier>();
+        }
+    }
+}
---
+++ Entity Framework Core - October 2019/10.XML Processing - Exercise/CarDealer/StartUp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+using CarDealer.Data;
+using CarDealer.Dtos.Import;
+using CarDealer.Models;
+
+using AutoMapper;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Mapper.Initialize(cfg => cfg.AddProfile<CarDealerProfile>());
+
+            using (var db = new CarDealerContext())
+            {
+                var inputXml = File.ReadAllText("./../../../Datasets/suppliers.xml");
+
+                var result = ImportSuppliers(db, inputXml);
+
+                Console.WriteLine(result);
+            }
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(ImportSupplierDto[]),
+                new XmlRootAttribute("Suppliers"));
+
+            ImportSupplierDto[] supplierDtos;
+
+            using (var reader = new StringReader(inputXml))
+            {
+                supplierDtos = (ImportSupplierDto[])xmlSerializer.Deserialize(reader);
+            }
+
+            var suppliers = Mapper.Map<Supplier[]>(supplierDtos);
+
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+
+
+            
+            return $"Successfully imported {suppliers.Length}";
+        }
+
+    }
+}
