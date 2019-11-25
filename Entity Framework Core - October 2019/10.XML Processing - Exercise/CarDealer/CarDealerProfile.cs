a6f57aaeecea2ff48946412f0257ab525471ff3a
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2019-11-25 17:02:58 +0100

    Adding more methods for Car Dealer Database

--- Entity Framework Core - October 2019/10.XML Processing - Exercise/CarDealer/CarDealerProfile.cs
+++ Entity Framework Core - October 2019/10.XML Processing - Exercise/CarDealer/CarDealerProfile.cs
@@ -1,7 +1,8 @@
 ﻿using CarDealer.Dtos.Import;
 using CarDealer.Models;
-
+using System.Linq;
 using AutoMapper;
+using CarDealer.Dtos.Export;
 
 namespace CarDealer
 {
@@ -10,6 +11,15 @@ namespace CarDealer
         public CarDealerProfile()
         {
             this.CreateMap<ImportSupplierDto, Supplier>();
+
+            this.CreateMap<ImportPartDto, Part>();
+
+            this.CreateMap<Supplier ,ExportLocalSuppliersDto>();
+
+            this.CreateMap<Part, ExportCarPartDto>();
+
+            this.CreateMap<Car, ExportCarDto>()
+                .ForMember(x => x.Parts, y => y.MapFrom(x => x.PartCars.Select(pc => pc.Part)));
         }
     }
 }
--- Entity Framework Core - October 2019/10.XML Processing - Exercise/CarDealer/StartUp.cs
+++ Entity Framework Core - October 2019/10.XML Processing - Exercise/CarDealer/StartUp.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
+using System.Text;
 using System.Xml.Serialization;
-
 using CarDealer.Data;
 using CarDealer.Dtos.Import;
 using CarDealer.Models;
 
 using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using CarDealer.Dtos.Export;
+using System.Collections.Generic;
 
 namespace CarDealer
 {
@@ -18,13 +22,15 @@ namespace CarDealer
 
             using (var db = new CarDealerContext())
             {
-                var inputXml = File.ReadAllText("./../../../Datasets/suppliers.xml");
+                //var inputXml = File.ReadAllText("./../../../Datasets/cars.xml");
 
-                var result = ImportSuppliers(db, inputXml);
+                var result = GetCarsWithTheirListOfParts(db);
 
                 Console.WriteLine(result);
             }
         }
+
+        //09 Problem - Import the suppliers from the provided file suppliers.xml.
 
         public static string ImportSuppliers(CarDealerContext context, string inputXml)
         {
@@ -43,10 +49,158 @@ namespace CarDealer
             context.Suppliers.AddRange(suppliers);
             context.SaveChanges();
 
-
-            
             return $"Successfully imported {suppliers.Length}";
         }
+
+        //10 Problem - Import the parts from the provided file parts.xml. If the supplierId doesn’t exists, skip the record.
+
+        public static string ImportParts(CarDealerContext context, string inputXml)
+        {
+            var xmlSerializer = 
+                new XmlSerializer(typeof(ImportPartDto[]),
+                new XmlRootAttribute("Parts"));
+
+            ImportPartDto[] partDtos;
+
+            //returns an object. Collection is needed to store the data.
+            using (var reader = new StringReader(inputXml))
+            {
+               partDtos = ((ImportPartDto[])xmlSerializer
+                    .Deserialize(reader))
+                    .Where(p => context.Suppliers.Any(s => s.Id == p.SupplierId))
+                    .ToArray(); 
+            }
+
+            var parts = Mapper.Map<Part[]>(partDtos);
+
+            context.Parts.AddRange(parts);
+            context.SaveChanges();
+
+
+            return $"Successfully imported {parts.Length}";
+        }
+
+        //11 Problem - Import cars from the provided file cars.xml. Select unique car part ids. If part id doesn’t exists, skip the part record.
+
+        public static string ImportCars(CarDealerContext context, string inputXml)
+        {
+            var xmlSerializer = 
+                new XmlSerializer(typeof(ImportCarDto[]), 
+                new XmlRootAttribute("Cars"));
+
+            ImportCarDto[] carDtos;
+
+            using (var reader = new StringReader(inputXml))
+            {
+                carDtos = ((ImportCarDto[])xmlSerializer.Deserialize(reader));
+            }
+
+            List<Car> cars = new List<Car>();
+            List<PartCar> partCars = new List<PartCar>();
+
+            foreach (var carDto in carDtos)
+            {
+                var car = new Car()
+                {
+                    Make = carDto.Make,
+                    Model = carDto.Model,
+                    TravelledDistance = carDto.TraveledDistance
+                };
+
+                var parts = carDto
+                    .Parts
+                    .Where(pdto => context.Parts.Any(p => p.Id == pdto.Id))
+                    .Select(p => p.Id)
+                    .Distinct();
+
+                foreach (var partId in parts)
+                {
+                    var partCar = new PartCar()
+                    {
+                        PartId = partId,
+                        Car = car
+                    };
+
+                    partCars.Add(partCar);
+                }
+
+                cars.Add(car);
+            }
+
+            context.Cars.AddRange(cars);
+            context.PartCars.AddRange(partCars);
+            context.SaveChanges();
+
+            return $"Successfully imported {cars.Count}";
+        }
+
+
+        //16 Get all suppliers that do not import parts from abroad. Get their id, name and the number of parts they can offer to supply.
+        public static string GetLocalSuppliers(CarDealerContext context)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            var suppliers = context
+                .Suppliers
+                .Where(s => !s.IsImporter)
+                .ProjectTo<ExportLocalSuppliersDto>()
+                .ToArray();
+
+            var xmlSerializer = 
+                new XmlSerializer(typeof(ExportLocalSuppliersDto[]), 
+                new XmlRootAttribute("suppliers"));
+
+            var namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(String.Empty, String.Empty);
+            
+            using(var writer = new StringWriter(stringBuilder))
+            {
+                xmlSerializer.Serialize(writer, suppliers, namespaces);
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+
+        //17 Problem  - Get all cars along with their list of parts.
+
+        public static string GetCarsWithTheirListOfParts(CarDealerContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            //Get all cars from database
+            var cars = context
+                .Cars
+                .OrderByDescending(c => c.TravelledDistance)
+                .ThenBy(c => c.Model)
+                .ProjectTo<ExportCarDto>()
+                .Take(5)
+                .ToArray();
+
+            foreach (var car in cars)
+            {
+                car.Parts = car.Parts
+                    .OrderByDescending(c => c.Price)
+                    .ToArray();
+            }
+
+            //Always start from the outer (first Dto)
+            var xmlSerializer = new XmlSerializer(typeof(ExportCarDto[]),
+                new XmlRootAttribute("cars"));
+
+            //Fixing namespaces in xml document. Don't forget to add them in xmlSerializer.Serialize().
+            var namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(String.Empty, String.Empty);
+
+            using (var writer = new StringWriter(sb))
+            {
+                xmlSerializer.Serialize(writer, cars, namespaces);
+            }
+           
+
+            return sb.ToString().TrimEnd();
+        }
+
+
 
     }
 }
