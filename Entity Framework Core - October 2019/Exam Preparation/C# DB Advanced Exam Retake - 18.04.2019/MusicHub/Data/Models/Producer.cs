83462cc0d322cc11689fd2cec2ef2ba3b88b4310
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2019-12-12 18:08:27 +0100

    MusicHub Database Model Definition.

---
+++ Entity Framework Core - October 2019/Exam Preparation/C# DB Advanced Exam Retake - 18.04.2019/MusicHub/Data/Models/Producer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicHub.Data.Models
+{
+    public class Producer
+    {
+        public Producer()
+        {
+            this.Albums = new HashSet<Album>();
+        }
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(3), MaxLength(30)]
+        public string Name { get; set; }
+
+        //Text consisting of two words separated with space 
+        //Each word must start with one upper letter and continue with many lower-case letters 
+        //(Example: "Bon Jovi")
+
+        [RegularExpression("[A-Z][a-z]+ [A-Z][a-z]+")]
+        public string Pseudonym { get; set; }
+
+        //text, consisting only of three groups (separated by space) 
+        //of three digits and starting always with "+359" 
+        //(Example: "+359 887 234 267")
+
+        [RegularExpression(@"\+359 [0-9]{3} [0-9]{3} [0-9]{3}")]
+        public string PhoneNumber { get; set; }
+
+        public ICollection<Album> Albums { get; set; }
+    }
+}
---
+++ Entity Framework Core - October 2019/Exam Preparation/C# DB Advanced Exam Retake - 18.04.2019/MusicHub/Data/Models/Writer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicHub.Data.Models
+{
+    public class Writer
+    {
+        public Writer()
+        {
+            this.Songs = new HashSet<Song>();
+        }
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(3),MaxLength(20)]
+        public string Name { get; set; }
+
+        //text, consisting of two words separated with space 
+        //and each word must start with one upper letter and continue with many lower-case letters 
+        //(Example: "Freddie Mercury")
+
+        [RegularExpression("[A-Z][a-z]+ [A-Z][a-z]+")]
+        public string Pseudonym { get; set; }
+
+        public ICollection<Song> Songs { get; set; }
+    }
+}
